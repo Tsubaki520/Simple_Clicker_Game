6eb1506ec34dfdcd021a34176d551e6028f9d82c
Tsubaki520 <Tsubaki520@gmail.com>
2019-04-19 13:22:10 +0800

    Create files.

---
+++ Simple_Clicker_Game/Assets/Scripts/ClickController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickController : MonoBehaviour
+{
+    [Header ("ClickSetting")]
+    [SerializeField]
+    private UnityEngine.UI.Text goldDisplay = null;
+    [SerializeField]
+    private UnityEngine.UI.Text dps = null;
+    public float gold = 0.00f;
+    public int goldperclick = 1;
+
+    void Start ()
+    {
+
+    }
+
+    void Update ()
+    {
+        goldDisplay.text = "Gold：" + gold;
+        dps.text = "DPS：" + goldperclick;
+    }
+
+    public void Clicked ()
+    {
+        gold += goldperclick;
+    }
+}
---
+++ Simple_Clicker_Game/Assets/Scripts/UpgradeManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour
+{
+    public ClickController click;
+    public UnityEngine.UI.Text itemInfo;
+    public float cost;
+    public int count = 0;
+    public int clickPower;
+    public string itemName;
+    private float m_newCost;
+
+    void Start ()
+    {
+
+    }
+
+    void Update ()
+    {
+        itemInfo.text = itemName + "\nCost：" + cost + "\nPower：+" + clickPower;
+    }
+
+    public void PurchasedUpgrade ()
+    {
+        if (click.gold >= cost)
+        {
+            click.gold -= cost;
+            count += 1;
+            click.goldperclick += clickPower;
+            cost = Mathf.Round (cost * 1.15f);
+            m_newCost = Mathf.Pow (cost, m_newCost = cost);
+        }
+    }
+}
